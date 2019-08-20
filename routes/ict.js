19a9c6feb934d787f6f833a2b77558b2bff1cd3b
mhlee <mhlee@ezfarm.co.kr>
2019-08-20 14:14:48 +0900

    테스트 이벤트 추가 moment를 이용하여 updateTm 추가

--- routes/ict.js
+++ routes/ict.js
@@ -1,4 +1,5 @@
 const _ = require('lodash'); // 오우~ 판타스틱!! lodash
+const moment = require('moment');
 
 function prettyJson(msg) {
   return JSON.stringify(msg, undefined, 2);
@@ -11,29 +12,71 @@ let doIt = function(req, res, next) {
   let msg = {
     type: 'res',
     farmNo: '1387',
-    icNo: 'I2',
+    icNo: 'I1',
     cmdString: [],
   };
 
   // -------------------------------
-  // 장비별 다중 메세지 설정
+  // 장비별 타입별 다중 메세지 설정
   // -------------------------------
-  str = { eqid: 'FS_SNR_0000000000025', report: [] };
-  evt = {
-    eleccur: '9.100',
-    igr: '50.000',
-    elecalarm: '0',
-    lgralarm: '2',
-    startTm: '20190418_071513',
-    endTm: '20190418_071513',
-    regTm: '20190418_071513',
+  e_str = { eqid: 'E1', report: [] };
+  e_evt = {
+    pigmnum: '100',
+    eatplan: '1',
+    setamt: '2',
+    eatamt: '3',
+    gyobaeday: '4',
+    lastentertm: '5',
+    eatrate: '6',
+    entercnt: '7',
+    elecnum: '8',
+    updateTm: '',
   };
+  randEvt(msg, e_str, e_evt);
 
-  report = str.report;
-  max = _.random(1, 10);
-  for (i = 0; i < max; i++) report.push(evt);
+  f_str = { eqid: 'F1', report: [] };
+  f_evt = {
+    pigmnum: '101',
+    eatplan: '11',
+    setamt: '12',
+    eatamt: '13',
+    gyobaeday: '14',
+    bunmanday: '15',
+    eatrate: '16',
+    updateTm: '',
+  };
+  randEvt(msg, f_str, f_evt);
 
-  msg.cmdString.push(str);
+  l_str = { eqid: 'L1', report: [] };
+  l_evt = {
+    aveday: '21',
+    feedamt: '22',
+    stkg: '23',
+    pigcnt: '24',
+    wateramt: '25',
+    wateramtsec: '26',
+    updateTm: '',
+  };
+  randEvt(msg, l_str, l_evt);
+
+  s_str = { eqid: 'S1', report: [] };
+  s_evt = {
+    temp: '31',
+    ventil: '32',
+    co2: '33',
+    Nh3: '34',
+    humi: '35',
+    illum: '36',
+    updateTm: '',
+  };
+  randEvt(msg, s_str, s_evt);
+
+  w_str = { eqid: 'W1', report: [] };
+  w_evt = {
+    warteramt: '41',
+    updateTm: '',
+  };
+  randEvt(msg, w_str, w_evt);
 
   // 응답 전송
   res.send(prettyJson(msg));
@@ -42,3 +85,15 @@ let doIt = function(req, res, next) {
 module.exports = {
   doIt: doIt,
 };
+
+function randEvt(msg, str, evt) {
+  now = moment();
+  ms = now.get('millisecond');
+  updateTm = now.format('YYYYMMDD_HHmm');
+  updateTm = updateTm + (ms % 60);
+  evt.updateTm = updateTm;
+  report = str.report;
+  max = _.random(1, 3);
+  _.times(max, report.push(evt));
+  msg.cmdString.push(str);
+}
